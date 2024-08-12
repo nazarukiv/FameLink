a198ec581be10213d718d8c8ab0d0ad2df5d61ef
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-08-12 15:38:10 +0100

    Add custom actions and display method to admin panel

--- popularpeople/admin.py
+++ popularpeople/admin.py
@@ -1,15 +1,30 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
 from popularpeople.models import People, Category, Partner
 
 
 @admin.register(People)
 class PeopleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_created', 'is_published', 'cat')
+    list_display = ('id', 'title', 'time_created', 'is_published', 'cat', 'brief_info')
     list_display_links = ('title','id')
     ordering = ['-time_created', 'title']
     list_editable = ('is_published', )
     list_per_page = 10
+    actions = ['set_published', 'set_draft']
+
+    @admin.display(description='Description')
+    def brief_info(self, people: People):
+        return f"Description {len(people.content)} symbols."
+
+    @admin.action(description="Publish chosen articles")
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=People.Status.PUBLISHED)
+        self.message_user(request, f"{count} articles changed ")
+
+    @admin.action(description="Unpublish chosen articles")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=People.Status.DRAFT)
+        self.message_user(request, f"{count} articles unpublished ", messages.WARNING)
 
 #admin.site.register(People, PeopleAdmin)
 
