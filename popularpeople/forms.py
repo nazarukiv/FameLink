eeb0183fe79a8644aaf3a5b61b8c4bfe559fd95a
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-08-13 15:05:39 +0100

    Validation added

--- popularpeople/forms.py
+++ popularpeople/forms.py
@@ -1,9 +1,14 @@
 from django import forms
+from django.core.validators import MaxLengthValidator, MinLengthValidator
+
 from .models import Category, Partner
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label="Title", widget=forms.TextInput(attrs={'class': 'form-input'}))
-    slug = forms.SlugField(max_length=255, label="URL")
+    title = forms.CharField(max_length=255, label="Title", widget=forms.TextInput(attrs={'class': 'form-input'}), min_length=5, error_messages={
+        "min_length": "At least 5 symbols",
+        "required": "Required**"
+    })
+    slug = forms.SlugField(max_length=255, label="URL", validators=[MinLengthValidator(5),MaxLengthValidator(100),])
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), label="Content", required=False)
     is_published = forms.BooleanField(required=False, label="Status")
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Category not chosen", initial=True,  label="Category")
